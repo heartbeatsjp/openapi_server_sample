135bbd402a9db0170446b308abfdd6e2561834e1
TATSUMI Soichiro <soichiro.tatsumi@heartbeats.jp>
2023-04-27 15:56:39 +0900

    ライセンスファイルを追加

--- openapi_server_sample/generate.py
+++ openapi_server_sample/generate.py
@@ -1,3 +1,7 @@
+# Copyright 2023 HEARTBEATS Corporation. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from main import app
 import json
 
--- openapi_server_sample/main.py
+++ openapi_server_sample/main.py
@@ -1,3 +1,7 @@
+# Copyright 2023 HEARTBEATS Corporation. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import random
 from fastapi import FastAPI
 from pydantic import BaseModel
